739e4d1d4d7a3a97b4ff1e4668508174d25701af
MishaTheMiner <mrkhaykin@gmail.com>
2013-06-14 10:27:25 -0700

    Scrolling Background

--- GitPractice/GitPractice/GitPractice/ScrollingBackground.cs
+++ GitPractice/GitPractice/GitPractice/ScrollingBackground.cs
@@ -4,27 +4,30 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
 
 namespace GitPractice
 {
     public sealed class ScrollingBackground : BaseSprite
     {
-        /* TODO: Michael
-         *
-         * Private Fields (aka variables): _scrollingSpeed
-         *
-         */
-
+        private Vector2 _scrollingSpeed;
         public Vector2 ScrollingSpeed { get; set; }
 
-        public void Update(GameTime gameTime, GameState gameState, MoveDirection scrollDirection)
-        { 
-            //TODO: Make this work
+        public void LoadContent(ContentManager content, string assetName, Vector2 scrollingSpeed)
+        {
+            _scrollingSpeed = scrollingSpeed;
+
+            base.LoadContent(content, assetName);      
         }
+
+        public void Update(GameTime gameTime, GameState gameState, MoveDirection scrollDirection)
+        {
+
+        }            
 
         public override void Draw(SpriteBatch spriteBatch)
         { 
-            //TODO: Make this work, too!
+            
         }
     }
 }
