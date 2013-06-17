d64109bc94db6290bfd94c75403614226f7fb370
CoolBots <stan@buildcoolrobots.com>
2013-06-17 12:53:08 -0700

    Moving PGCGame to Master branch

---
+++ PGCGame/PGCGame/PGCGame/Drone.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PGCGame
+{
+    public class Drone : Ship
+    {
+        public int ShotsFromMain { get; set; }
+        //must add event from fighter carrier
+    }
+}
---
+++ PGCGame/PGCGame/PGCGame/EMP.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PGCGame
+{
+    public class EMP : SecondaryWeapon
+    {
+        public int Radius { get; set; }
+        public int Diameter
+        {
+            get
+            {
+                return Radius * 2;
+            }
+            set
+            {
+                Radius = value / 2;
+            }
+        }
+
+        public Ship LaunchingShip { get; set; }
+    }
+}
