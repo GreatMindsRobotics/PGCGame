388aaa23f913c0aa30c1d5c081dea35cfb9da6cf
TheBudderKing <mjpiamonte@verizon.net>
2013-06-19 10:39:46 -0700

    After Credits it goes back to Title Screen

--- PGCGame/PGCGame/PGCGame/Screens/Credits.cs
+++ PGCGame/PGCGame/PGCGame/Screens/Credits.cs
@@ -22,6 +22,9 @@ namespace PGCGame.Screens
         private Song _creditsSong;
         private Vector2 _scrollingSpeed;
 
+        private TimeSpan _timeUntilCreditsFinish = new TimeSpan (0, 0, 0, 45, 0);
+        private TimeSpan _elapsedTime;
+
 
         public Credits(SpriteBatch spriteBatch)
             : base(spriteBatch, Color.Black)
@@ -35,21 +38,30 @@ namespace PGCGame.Screens
             
             credits = new TextSprite(Sprites.SpriteBatch, content.Load<SpriteFont>("Fonts\\SegoeUIMono"), "   Plequarius: Galactic Commanders\n\n\n\n\n\nWeek 1 - Functional Spec, GameState Management\n\n\n\n- Kai F.\n\n\n- Michael K.\n\n\n- Alexa L.\n\n\n- Andrea L.\n\n\n- Alexander L.\n\n\n- Matthew P.\n\n\n- Jeremiah T.\n\n\n\n\nWeek 2 - Technical Spec, Class Design, Functionality\n\n\n\n- Glen H.\n\n\n- Michael K.\n\n\n- Alex L.\n\n\n- Matthew P.\n\n\n\n\n\n\n Week 3 - AI's\n\n\n\n\n\n\n\n\n\n\n\nWeek 4 - Xbox Converson\n\n\n\n\nUnderlying Library written by:\nGlen Husman (glen3b)\nGlib is available on github! \n\n\n\n\n\n\n                Music:\n\nFailing Defense - Kevin MacLeod\n\nAll music obtained from Incompetech.com", Color.White);
 
-            credits.Position = new Vector2(200, 600);
+            credits.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * .255f, Sprites.SpriteBatch.GraphicsDevice.Viewport.Height);
 
             _creditsSong = content.Load<Song>("Songs\\Failing Defense");
 
             AdditionalSprites.Add(credits);
+            _elapsedTime = new TimeSpan();
         }
 
         public override void Update(GameTime gameTime)
         {
+            _elapsedTime += gameTime.ElapsedGameTime;
             base.Update(gameTime);
             if (MediaPlayer.State != MediaState.Playing)
             {
                 MediaPlayer.Play(_creditsSong);
             }
             credits.Position += _scrollingSpeed;
+
+            if (_elapsedTime >= _timeUntilCreditsFinish)
+            {
+                MediaPlayer.Stop();
+                StateManager.ScreenState = ScreenState.Title;
+            }
+
             //TODO: UPDATE SPRITES
         }
     }
