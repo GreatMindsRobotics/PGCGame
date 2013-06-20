41a85a2ac3958d03ffe8b4fe06c633cd20e57afd
TheBudderKing <mjpiamonte@verizon.net>
2013-06-20 10:49:26 -0700

    Working on pause Screen

--- PGCGame/PGCGame/PGCGame/Screens/PauseScreen.cs
+++ PGCGame/PGCGame/PGCGame/Screens/PauseScreen.cs
@@ -17,15 +17,37 @@ namespace PGCGame.Screens
 {
     public class PauseScreen : Screen
     {
+        TextSprite PauseLabel;
+        bool mouseInBackButton = false;
         public PauseScreen(SpriteBatch spriteBatch)
             : base(spriteBatch, Color.Black)
+
         {
             
         }
 
         public void LoadContent(ContentManager content)
         {
+            Sprite BackButton = new Sprite(content.Load<Texture2D>("Images\\Controls\\Button"), new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * .06f, Sprites.SpriteBatch.GraphicsDevice.Viewport.Height * .60f), Sprites.SpriteBatch);
+            PauseLabel = new TextSprite(Sprites.SpriteBatch, new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * .139f, Sprites.SpriteBatch.GraphicsDevice.Viewport.Height * .62f), content.Load<SpriteFont>("Fonts\\SegoeUIMono"), "Back");
+            PauseLabel.Color = Color.White;
+            Sprite PauseButton = new Sprite(content.Load<Texture2D>("Images\\Controls\\Button"), new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * .06f, Sprites.SpriteBatch.GraphicsDevice.Viewport.Height * .60f), Sprites.SpriteBatch);
+            PauseLabel = new TextSprite(Sprites.SpriteBatch, new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * .139f, Sprites.SpriteBatch.GraphicsDevice.Viewport.Height * .62f), content.Load<SpriteFont>("Fonts\\SegoeUIMono"), "Back");
+            PauseButton.MouseEnter += new EventHandler(PauseButton_MouseEnter);
+            PauseButton.MouseLeave += new EventHandler(PauseButton_MouseLeave);
 
+
+
+        }
+
+        void PauseButton_MouseLeave(object sender, EventArgs e)
+        {
+            
+        }
+
+        void PauseButton_MouseEnter(object sender, EventArgs e)
+        {
+            
         }
 
         public override void Update(GameTime gameTime)
