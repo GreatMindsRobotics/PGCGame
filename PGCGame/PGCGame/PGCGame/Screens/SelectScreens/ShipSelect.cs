a25a1b8d62c938f81aa5d8e57798b0260f88e33d
alex45101 <aleontiev00@gmail.com>
2013-06-21 14:48:50 -0700

    Added more things to ship select screen

--- PGCGame/PGCGame/PGCGame/Screens/SelectScreens/ShipSelect.cs
+++ PGCGame/PGCGame/PGCGame/Screens/SelectScreens/ShipSelect.cs
@@ -30,22 +30,24 @@ namespace PGCGame.Screens
         public override void LoadContent(ContentManager content)
         {
             ship1 = new Sprite(content.Load<Texture2D>("Images\\Battle Cruiser\\Tier1"), Vector2.Zero, Sprites.SpriteBatch);
-            TextSprite text1 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, content.Load<SpriteFont>("Fonts\\SegoeUIMono"), "TODO");
+            TextSprite text1 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, content.Load<SpriteFont>("Fonts\\SegoeUIMono"), " Battle Cruiser\n\n This is the srtrongest class \n in the fleet, but also the slowest.\n What it lacks in speed it makes \n up for in strength.\n\n Damage Per Shot: 20\n Amount of Health: 120");
             text1.Color = Color.White;
             ship1.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.81f, Sprites.SpriteBatch.GraphicsDevice.Viewport.Height * .12f);
             ship1.Rotation = new SpriteRotation(90);
 
+
             items.Add(new KeyValuePair<Sprite, TextSprite>(ship1, text1));
+           
 
             ship2 = new Sprite(content.Load<Texture2D>("Images\\Fighter Carrier\\Tier1"), Vector2.Zero, Sprites.SpriteBatch);
-            TextSprite text2 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, content.Load<SpriteFont>("Fonts\\SegoeUIMono"), "TODO");
+            TextSprite text2 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, content.Load<SpriteFont>("Fonts\\SegoeUIMono"), " Fighter Carrier\n\n This class fires an extremely fast\n Flak Cannon and has the ability to\n deploy drones. However, the drones\n and Flak Cannon aren't that powerful.\n After the Carrier gets destroyed, the\n drones die with it.\n\n Damage Per Shot: 2\n Amount of Health: 100\n Amount of Drones: 2\n Damage Per Drone Shot: 1\n Health Per Drone: 10");
             ship2.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.85f, Sprites.SpriteBatch.GraphicsDevice.Viewport.Height * .01f);
             ship2.Rotation = new SpriteRotation(90);
 
             items.Add(new KeyValuePair<Sprite, TextSprite>(ship2, text2));
 
             ship3 = new Sprite(content.Load<Texture2D>("Images\\Torpedo Ship\\Tier1"), Vector2.Zero, Sprites.SpriteBatch);
-            TextSprite text3 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, content.Load<SpriteFont>("Fonts\\SegoeUIMono"), "TODO");
+            TextSprite text3 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, content.Load<SpriteFont>("Fonts\\SegoeUIMono"), " Torpedo Ship\n\n This class is the most balanced\n ship in the game The torpedos do\n a lot of damage and the torpedos\n are hard to dodge!\n\n Damege Per Shot: 5\n Amount of Health: 110");
             ship3.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.81f, Sprites.SpriteBatch.GraphicsDevice.Viewport.Height * .12f);
             ship3.Rotation = new SpriteRotation(90);
 
