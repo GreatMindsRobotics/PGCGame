996908e100d63237c00712d3225d35fd1d2a00c2
BenGMR <villalobosb93@gmail.com>
2013-06-21 19:18:12 -0700

    Adding the upgradescreen class itself

---
+++ PGCGame/PGCGame/PGCGame/Screens/SelectScreens/UpgradeScreen.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+using Glib;
+using Glib.XNA;
+using Glib.XNA.SpriteLib;
+
+namespace PGCGame.Screens.SelectScreens
+{
+    class UpgradeScreen : BaseSelectScreen
+    {
+       public UpgradeScreen(SpriteBatch spriteBatch)
+       : base(spriteBatch)
+        {
+            
+        }
+
+       public override void LoadContent(ContentManager content)
+       {
+           //TODO: Load upgrade sprites and description of upgrades.
+
+           base.LoadContent(content);
+       }
+
+
+       public override void Update(GameTime gameTime)
+       {
+           
+           base.Update(gameTime);
+       }
+    }
+}
