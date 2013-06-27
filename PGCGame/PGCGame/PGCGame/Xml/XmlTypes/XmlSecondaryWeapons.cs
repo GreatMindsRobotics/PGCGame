defabd2c778172337d321bac825e5d330d375d03
alex45101 <aleontiev00@gmail.com>
2013-06-27 12:10:56 -0700

    Adding spacemine image and xml secondary weapon
    secondary weapon in not ready

--- PGCGame/PGCGame/PGCGame/Xml/XmlTypes/XmlSecondaryWeapons.cs
+++ PGCGame/PGCGame/PGCGame/Xml/XmlTypes/XmlSecondaryWeapons.cs
@@ -2,10 +2,35 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
+using Glib;
 
 namespace PGCGame.Xml.XmlTypes
 {
     public class XmlSecondaryWeapons : XmlBaseLoader
     {
+        public struct Weapon
+        {
+            public int ID { get; set; }
+            public string Cost { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+        }
+
+
+        public List<Weapon> Weapons;
+
+        public void Load()
+        { 
+            foreach(XmlElement element in _xml.GetElementsByTagName("weapon"))
+            {
+                Weapon weapon = new Weapon();
+
+                weapon.ID = element.Attributes.GetNamedItem("id").Value.ToInt();
+                weapon.Name = element.GetElementsByTagName("name")[0].InnerText;
+                weapon.Description = element.GetElementsByTagName("description")[0].InnerText;
+                weapon.Cost = element.GetElementsByTagName("Cost")[0].InnerText; 
+            }
+        }
     }
 }
