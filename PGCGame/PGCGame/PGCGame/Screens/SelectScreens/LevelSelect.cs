3e28008c88d5047385c9bdc3f110c56eef6bdd3b
slimenx <alatasaf@gmail.com>
2013-06-28 11:59:26 -0700

    Added level select class for a student to work on

---
+++ PGCGame/PGCGame/PGCGame/Screens/SelectScreens/LevelSelect.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Glib.XNA.SpriteLib;
+using Glib.XNA;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+
+namespace PGCGame.Screens.SelectScreens
+{
+    public class LevelSelect : BaseSelectScreen
+    {
+        public LevelSelect(SpriteBatch spriteBatch)
+            : base(spriteBatch)
+        {
+
+        }
+
+        public override void LoadContent(ContentManager content)
+        {
+            //TODO FOR MATTHEW 
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            base.Update(gameTime);
+        }
+    }
+}
