4670bffe83b302ed163ec2143cf512a46c838440
legoman5746 <joe.vassallo@roadrunner.com>
2013-06-28 13:36:50 -0700

    fixed spelling mistake
    damage not damege

--- PGCGame/PGCGame/PGCGame/Screens/SelectScreens/ShipSelect.cs
+++ PGCGame/PGCGame/PGCGame/Screens/SelectScreens/ShipSelect.cs
@@ -49,7 +49,7 @@ namespace PGCGame.Screens
             items.Add(new KeyValuePair<Sprite, TextSprite>(ship2, text2));
 
             ship3 = new Sprite(content.Load<Texture2D>("Images\\Torpedo Ship\\Tier1"), Vector2.Zero, Sprites.SpriteBatch);
-            TextSprite text3 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, content.Load<SpriteFont>("Fonts\\SegoeUIMono"), " Torpedo Ship\n\n This class is the most balanced\n ship in the game. The torpedos do\n a lot of damage and \n are hard to dodge!\n\n Damege Per Shot: 5\n Amount of Health: 110");
+            TextSprite text3 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, content.Load<SpriteFont>("Fonts\\SegoeUIMono"), " Torpedo Ship\n\n This class is the most balanced\n ship in the game. The torpedos do\n a lot of damage and \n are hard to dodge!\n\n Damage Per Shot: 5\n Amount of Health: 110");
             ship3.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.81f, Sprites.SpriteBatch.GraphicsDevice.Viewport.Height * .12f);
             ship3.Rotation = new SpriteRotation(90);
 
