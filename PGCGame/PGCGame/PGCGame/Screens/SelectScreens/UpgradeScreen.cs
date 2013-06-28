5f504c2cc2b1ebfb74b5f12d81b9f6279b487391
kdckdc <kfish123@charter.net>
2013-06-28 11:38:54 -0700

    Work in progress upgradescreen

--- PGCGame/PGCGame/PGCGame/Screens/SelectScreens/UpgradeScreen.cs
+++ PGCGame/PGCGame/PGCGame/Screens/SelectScreens/UpgradeScreen.cs
@@ -25,11 +25,30 @@ namespace PGCGame.Screens.SelectScreens
 
        public override void LoadContent(ContentManager content)
        {
-           //TODO: Load upgrade sprites and description of upgrades.
+           Texture2D tempImage = content.Load<Texture2D>("Images\\NonPlayingObject\\Planet");
+           SpriteFont font = content.Load<SpriteFont>("Fonts\\SegoeUIMono");
 
+           Sprite image = new Sprite(tempImage, Vector2.Zero, Sprites.SpriteBatch);
+           TextSprite text = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, font, "TODO");
+           text.Color = Color.White;
+           image.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.6f, 50);
+           text.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.1f, 30);
+           items.Add(new KeyValuePair<Sprite, TextSprite>(image, text));
+           
+           Sprite image2 = new Sprite(tempImage, Vector2.Zero, Sprites.SpriteBatch);
+           TextSprite text2 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, font, "todo");
+           image2.Scale = new Vector2(0.5f, 0.5f);
+           image2.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.66f, 100);
+           text2.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.1f, 30);
+           items.Add(new KeyValuePair<Sprite, TextSprite>(image2, text2));
+           Sprite image3 = new Sprite(tempImage, Vector2.Zero, Sprites.SpriteBatch);
+           TextSprite text3 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, font, "todo");
+           text2.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.1f, 30);
+           image3.Scale = new Vector2(0.75f,0.75f);
+           image3.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.66f, 100);
+        
            base.LoadContent(content);
        }
-
 
        public override void Update(GameTime gameTime)
        {
