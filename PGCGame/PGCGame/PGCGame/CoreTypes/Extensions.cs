20a22e998b590b067fd85d3612df6d79cef85338
Glen Husman <wappokickingdeer@gmail.com>
2013-06-30 08:36:03 -0700

    Added remarks and improvements to random vector.

--- PGCGame/PGCGame/PGCGame/CoreTypes/Extensions.cs
+++ PGCGame/PGCGame/PGCGame/CoreTypes/Extensions.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Glib;
 using Microsoft.Xna.Framework;
 
 namespace PGCGame.CoreTypes
@@ -34,6 +35,10 @@ namespace PGCGame.CoreTypes
         /// <param name="random">Random number generator to use</param>
         /// <param name="minValue">Inclusive minimum value</param>
         /// <param name="maxValue">Exclusive maximum value</param>
+        /// <remarks>
+        /// Vectors are rounded to integers during randomization... floating point minimums and maximums won't work.
+        /// The returned Vector2 will not be floating point, it will have an integer-convertible value (so no return value of .5 anything).
+        /// </remarks>
         /// <returns>new Vector2 object. If minValue.X is equal to maxValue.X, only Y-axis is randomized; if minValue.Y is equal to maxValue.Y, only X-axis is randomized. Both X and Y components cannot be equal in minValue and maxValue.</returns>
         public static Vector2 NextVector2(this Random random, Vector2 minValue, Vector2 maxValue)
         {
@@ -50,8 +55,8 @@ namespace PGCGame.CoreTypes
                 throw new Exception("minValue cannot equal maxValue");
             }
 
-            float x = minValue.X == maxValue.X ? minValue.X : (float)random.Next((int)minValue.X, (int)maxValue.X);
-            float y = minValue.Y == maxValue.Y ? minValue.Y : (float)random.Next((int)minValue.Y, (int)maxValue.Y);
+            float x = minValue.X == maxValue.X ? minValue.X : random.Next(minValue.X.Round(), maxValue.X.Round()).ToFloat();
+            float y = minValue.Y == maxValue.Y ? minValue.Y : random.Next(minValue.Y.Round(), maxValue.Y.Round()).ToFloat();
 
             return new Vector2(x, y);
         }
