b29baf953e31e6d8c78bb8b907307c786dd1eb80
alex45101 <aleontiev00@gmail.com>
2013-07-08 14:54:47 -0700

    Add level select screen and checked other screens
    Closed the issues that were assigned to me

--- PGCGame/PGCGame/PGCGame/Screens/SelectScreens/UpgradeScreen.cs
+++ PGCGame/PGCGame/PGCGame/Screens/SelectScreens/UpgradeScreen.cs
@@ -24,6 +24,10 @@ namespace PGCGame.Screens.SelectScreens
             
         }
 
+       TextSprite text;
+       TextSprite text2;
+       TextSprite text3;
+
        public override void InitScreen(ScreenType screenType)
        {
            base.InitScreen(screenType);
@@ -32,30 +36,32 @@ namespace PGCGame.Screens.SelectScreens
            SpriteFont font = GameContent.GameAssets.Fonts.NormalText;
 
            Sprite image = new Sprite(tempImage, Vector2.Zero, Sprites.SpriteBatch);
-           TextSprite text = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, font, "TODO");
+           text = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, font, "TODO");
            text.Color = Color.White;
            image.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.6f, 50);
            text.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.1f, 30);
            items.Add(new KeyValuePair<Sprite, TextSprite>(image, text));
            
            Sprite image2 = new Sprite(tempImage, Vector2.Zero, Sprites.SpriteBatch);
-           TextSprite text2 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, font, "Todo");
+           text2 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, font, "Todo");
            image2.Scale = new Vector2(0.5f, 0.5f);
            image2.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.66f, 150);
            text2.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.1f, 30);
+           text2.Color = Color.White;
            items.Add(new KeyValuePair<Sprite, TextSprite>(image2, text2));
+
            Sprite image3 = new Sprite(tempImage, Vector2.Zero, Sprites.SpriteBatch);
-           TextSprite text3 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, font, "todo too");
+           text3 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, font, "todo too");
            text3.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.1f, 30);
            image3.Scale = new Vector2(0.75f,0.75f);
            image3.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.63f, 100);
            items.Add(new KeyValuePair<Sprite, TextSprite>(image3, text3));
 
+           acceptLabel.Text = "Buy";
        }
 
        public override void Update(GameTime gameTime)
        {
-           
            base.Update(gameTime);
        }
     }
