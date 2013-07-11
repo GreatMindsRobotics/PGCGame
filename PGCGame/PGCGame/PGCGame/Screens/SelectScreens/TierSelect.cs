a435af67908e837fd73558b0880582327739c7ac
MishaTheMiner <mrkhaykin@gmail.com>
2013-07-11 10:04:24 -0700

    You can purchase ships now, however..
    the cost is set to 100k due to an issue with the "Cost" property somehow
    not being available in the ship object.

--- PGCGame/PGCGame/PGCGame/Screens/SelectScreens/TierSelect.cs
+++ PGCGame/PGCGame/PGCGame/Screens/SelectScreens/TierSelect.cs
@@ -25,60 +25,68 @@ namespace PGCGame.Screens.SelectScreens
         }
 
         //This screen needs sprites for each tier of ship and descriptions for each tier.
-        List<KeyValuePair<Sprite, string>> itemsShown = new List<KeyValuePair<Sprite, string>>();
+        List<Ship> itemsShown = new List<Ship>();
 
         public override void InitScreen(ScreenType screenType)
           {
-            Sprite ship1;
-            Sprite ship2;
-            Sprite ship3;
+            Ship BattleCruiser;
+            Ship FighterCarreer;
+            Ship TorpedoShip;
             Texture2D buttonImage = GameContent.GameAssets.Images.Controls.Button;
             SpriteFont SegoeUIMono = GameContent.GameAssets.Fonts.NormalText;
 
             
-            ship1 = new Sprite(GameContent.GameAssets.Images.Ships[ShipType.BattleCruiser, ShipTier.Tier2], Vector2.Zero, Sprites.SpriteBatch);
+            BattleCruiser = new BattleCruiser(GameContent.GameAssets.Images.Ships[ShipType.BattleCruiser, ShipTier.Tier2], Vector2.Zero, Sprites.SpriteBatch);
             TextSprite text1 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, GameContent.GameAssets.Fonts.NormalText, "\n\n This is the strongest class \n in the fleet, but also the slowest.\n What it lacks in speed it makes \n up for in strength.\n\n Damage Per Shot: 20\n Amount of Health: 120");
             text1.Color = Color.White;
-            ship1.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.81f, Sprites.SpriteBatch.GraphicsDevice.Viewport.Height * .12f);
-            ship1.Rotation = new SpriteRotation(90);
+            BattleCruiser.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.81f, Sprites.SpriteBatch.GraphicsDevice.Viewport.Height * .12f);
+            BattleCruiser.Rotation = new SpriteRotation(90);
 
-            items.Add(new KeyValuePair<Sprite, TextSprite>(ship1, text1));
-            itemsShown.Add(new KeyValuePair<Sprite, string>(ship1, "Battle Cruiser"));
+            items.Add(new KeyValuePair<Sprite, TextSprite>(BattleCruiser, text1));
+            itemsShown.Add(BattleCruiser);
 
 
-            ship2 = new Sprite(GameContent.GameAssets.Images.Ships[ShipType.FighterCarrier, ShipTier.Tier2], Vector2.Zero, Sprites.SpriteBatch);
+            FighterCarreer = new FighterCarrier(GameContent.GameAssets.Images.Ships[ShipType.FighterCarrier, ShipTier.Tier2], Vector2.Zero, Sprites.SpriteBatch, GameContent.GameAssets.Images.Ships[ShipType.Drone, ShipTier.Tier1]);
             TextSprite text2 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, GameContent.GameAssets.Fonts.NormalText, "\n\n This class fires an extremely fast\n Flak Cannon and has the ability to\n deploy drones. However, the drones\n and Flak Cannon aren't that powerful.\n After the Carrier gets destroyed, the\n drones die with it.\n\n Damage Per Shot: 2\n Amount of Health: 100\n Amount of Drones: 2\n Damage Per Drone Shot: 1\n Health Per Drone: 10");
-            ship2.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.85f, Sprites.SpriteBatch.GraphicsDevice.Viewport.Height * .01f);
-            ship2.Rotation = new SpriteRotation(90);
+            FighterCarreer.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.85f, Sprites.SpriteBatch.GraphicsDevice.Viewport.Height * .01f);
+            FighterCarreer.Rotation = new SpriteRotation(90);
 
-            items.Add(new KeyValuePair<Sprite, TextSprite>(ship2, text2));
-            itemsShown.Add(new KeyValuePair<Sprite, string>(ship2, "Fighter Carrier"));
+            items.Add(new KeyValuePair<Sprite, TextSprite>(FighterCarreer, text2));
+            itemsShown.Add(FighterCarreer);
 
-            ship3 = new Sprite(GameContent.GameAssets.Images.Ships[ShipType.TorpedoShip, ShipTier.Tier2], Vector2.Zero, Sprites.SpriteBatch);
+            TorpedoShip = new TorpedoShip(GameContent.GameAssets.Images.Ships[ShipType.TorpedoShip, ShipTier.Tier2], Vector2.Zero, Sprites.SpriteBatch);
             TextSprite text3 = new TextSprite(Sprites.SpriteBatch, Vector2.Zero, GameContent.GameAssets.Fonts.NormalText, "\n\n This class is the most balanced\n ship in the game. The torpedos do\n a lot of damage and \n are hard to dodge!\n\n Damege Per Shot: 5\n Amount of Health: 110");
-            ship3.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.81f, Sprites.SpriteBatch.GraphicsDevice.Viewport.Height * .12f);
-            ship3.Rotation = new SpriteRotation(90);
+            TorpedoShip.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * 0.81f, Sprites.SpriteBatch.GraphicsDevice.Viewport.Height * .12f);
+            TorpedoShip.Rotation = new SpriteRotation(90);
 
-            items.Add(new KeyValuePair<Sprite, TextSprite>(ship3, text3));
-            itemsShown.Add(new KeyValuePair<Sprite, string>(ship3, "Torpedo Ship"));
+            items.Add(new KeyValuePair<Sprite, TextSprite>(TorpedoShip, text3));
+            itemsShown.Add(TorpedoShip);
 
             ChangeItem += new EventHandler(TierSelect_ChangeItem);
+            nextButtonClicked += new EventHandler(TierSelect_nextButtonClicked);
 
             base.InitScreen(screenType);
 
             acceptLabel.Text = "Buy";
         }
 
-        void TierSelect_ChangeItem(object sender, EventArgs e)
+        void TierSelect_nextButtonClicked(object sender, EventArgs e)
         {
-            foreach (KeyValuePair<Sprite, string> item in itemsShown)
+            foreach (Ship ship in itemsShown)
             {
-                if (item.Key == items[selected].Key)
+                if (ship.Texture == items[selected].Key.Texture)
                 {
-                    nameLabel.Text = item.Value;
-                    break;
+                    
+                    
+                    StateManager.SpaceBucks -= 100000;
+                   
                 }
             }
+        }
+
+        void TierSelect_ChangeItem(object sender, EventArgs e)
+        {
+            
         }
 
         public override void Update(GameTime gameTime)
