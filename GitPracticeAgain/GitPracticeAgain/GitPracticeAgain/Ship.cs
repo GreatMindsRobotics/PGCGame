8a1423420dee6c6299c47e0fe6f07bc7ef29e302
binkers <brandon98@gmail.com>
2013-07-26 12:31:34 -0700

    Ship class still WIP

--- GitPracticeAgain/GitPracticeAgain/GitPracticeAgain/Ship.cs
+++ GitPracticeAgain/GitPracticeAgain/GitPracticeAgain/Ship.cs
@@ -2,11 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
 
 namespace GitPracticeAgain
 {
-    class Ship
+    class Ship : BaseSprite
     {
+        private Vector2 Speed = new Vector2(0,0);
+        
+        
+        public void Update(KeyboardState keyboardState)
+        {
 
+        }
+
+
+        
+        private void move(KeyboardState keyboardState)
+        {
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                Speed.X = 3;
+            }
+            else if (keyboardState.IsKeyDown(Keys.Up))
+            {
+
+            }
+        }
     }
 }
