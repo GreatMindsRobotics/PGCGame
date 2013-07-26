259160daada27426699cf1b7d0b461d987e67e68
BenGMR <villalobosb93@gmail.com>
2013-07-26 10:20:47 -0700

    Added project and base sprite class
    This gitpractice is for ross, brandon, and rehaan

---
+++ GitPracticeAgain/GitPracticeAgain/GitPracticeAgain/BaseSprite.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GitPracticeAgain
+{
+    class BaseSprite
+    {
+        private Vector2 _position;
+
+        public Vector2 Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+        private Texture2D _texture;
+
+        public Texture2D Texture
+        {
+            get { return _texture; }
+            set { _texture = value; }
+        }
+
+        private Color _tintColor;
+
+        public Color Color
+        {
+            get { return _tintColor; }
+            set { _tintColor = value; }
+        }
+
+        public void LoadContent(Vector2 position, Texture2D texture, Color color)
+        {
+            _position = position;
+            _texture = texture;
+            _tintColor = color;
+        }
+
+        public void Draw(SpriteBatch batch)
+        {
+            batch.Draw(_texture, _position, _tintColor);
+        }
+        
+
+    }
+}
