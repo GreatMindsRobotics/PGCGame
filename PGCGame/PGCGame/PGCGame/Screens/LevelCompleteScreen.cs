911f50f86892a39f84581bd682d98c1680d223ac
MishaTheMiner <mrkhaykin@gmail.com>
2013-08-06 11:45:34 -0700

    Fixed Bug in LEVELCOMPLETESCREEN

--- PGCGame/PGCGame/PGCGame/Screens/LevelCompleteScreen.cs
+++ PGCGame/PGCGame/PGCGame/Screens/LevelCompleteScreen.cs
@@ -20,12 +20,22 @@ namespace PGCGame
         public override void InitScreen(ScreenType screenName)
         {
             this.BackgroundSprite = HorizontalMenuBGSprite.CurrentBG;
+            Sprite Button;
+            Texture2D ButtonImage = GameContent.GameAssets.Images.Controls.Button;
+            Vector2 ButtonPosition = new Vector2(155);
+            Color tintColor;
+            Button = new Sprite(ButtonImage, ButtonPosition, Sprites.SpriteBatch);
+            AdditionalSprites.Add(Button);
             
+
             TextSprite Continue = new TextSprite(Sprites.SpriteBatch, GameContent.GameAssets.Fonts.NormalText, string.Format("Continue"));
             Continue.Color = Color.Beige;
+            Continue.ParentSprite = Button;
             AdditionalSprites.Add(Continue);
+            Continue.Pressed += new EventHandler(Continue_Pressed);
             
-            TextSprite winText = new TextSprite(Sprites.SpriteBatch, GameContent.GameAssets.Fonts.NormalText, string.Format("{0} Completed!\nYou earned {1} Points\nYou Have {2} earned SpaceBucks ",StateManager.SpaceBucks, StateManager.SpacePoints, StateManager.CurrentLevel));
+            
+            TextSprite winText = new TextSprite(Sprites.SpriteBatch, GameContent.GameAssets.Fonts.NormalText, string.Format("{2} Completed!\nYou earned {1} Points\nYou Have {0} earned SpaceBucks ",StateManager.SpaceBucks, StateManager.SpacePoints, StateManager.CurrentLevel));
             winText.Color = Color.Beige;
             AdditionalSprites.Add(winText);
 
@@ -33,6 +43,12 @@ namespace PGCGame
 
         }
 
+        void Continue_Pressed(object sender, EventArgs e)
+        {
+            StateManager.ScreenState = CoreTypes.ScreenType.MainMenu;
+        }
+
+       
         public override void Update(GameTime game)
         {
             base.Update(game);
