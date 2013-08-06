08c001c5215b0a2ae49425f9b81535482bc517ae
MishaTheMiner <mrkhaykin@gmail.com>
2013-08-06 14:25:42 -0700

    Completed LevelCompleteScreen

--- PGCGame/PGCGame/PGCGame/Screens/LevelCompleteScreen.cs
+++ PGCGame/PGCGame/PGCGame/Screens/LevelCompleteScreen.cs
@@ -32,13 +32,17 @@ namespace PGCGame
             Continue.Color = Color.Beige;
             Continue.ParentSprite = Button;
             AdditionalSprites.Add(Continue);
-            Continue.Pressed += new EventHandler(Continue_Pressed);
+            Continue.Pressed  += new EventHandler(delegate(object src, EventArgs e) { if (this.Visible) { StateManager.ScreenState  = CoreTypes.ScreenType.MainMenu; } });;
+            Continue.IsHoverable = true;
+            Continue.HoverColor = Color.Navy;
+            Button.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * .40f, 200);
+           
             
-            
-            TextSprite winText = new TextSprite(Sprites.SpriteBatch, GameContent.GameAssets.Fonts.NormalText, string.Format("{2} Completed!\nYou earned {1} Points\nYou Have {0} earned SpaceBucks ",StateManager.SpaceBucks, StateManager.SpacePoints, StateManager.CurrentLevel));
+            TextSprite winText = new TextSprite(Sprites.SpriteBatch, GameContent.GameAssets.Fonts.NormalText, string.Format("{2} Completed!\n\nYou earned {1} Points\n\nYou Have {0} earned SpaceBucks ",StateManager.SpaceBucks, StateManager.SpacePoints, StateManager.CurrentLevel));
             winText.Color = Color.Beige;
             AdditionalSprites.Add(winText);
-
+             winText.Position = new Vector2(Sprites.SpriteBatch.GraphicsDevice.Viewport.Width * .30f, 50);
+           
             base.InitScreen(screenName);
 
         }
