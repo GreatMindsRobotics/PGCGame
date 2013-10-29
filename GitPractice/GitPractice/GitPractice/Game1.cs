9e8bc617cc2f690a24e3e5bc36309e2011e24e75
slimenx <alatasaf@gmail.com>
2013-10-29 16:34:46 -0700

    changed bg color

--- GitPractice/GitPractice/GitPractice/Game1.cs
+++ GitPractice/GitPractice/GitPractice/Game1.cs
@@ -217,7 +217,7 @@ namespace GitPractice
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Black);
+            GraphicsDevice.Clear(Color.Aqua);
 
             spriteBatch.Begin();
 
