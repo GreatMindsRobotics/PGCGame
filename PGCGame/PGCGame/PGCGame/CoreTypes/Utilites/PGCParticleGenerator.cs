9f72b2bee1b9cd123454c5bf1256b78ba8c88abd
Glen Husman <wappokickingdeer@gmail.com>
2013-12-27 13:30:02 -0800

    Updated particle engine to use superclass methods

--- PGCGame/PGCGame/PGCGame/CoreTypes/Utilites/PGCParticleGenerator.cs
+++ PGCGame/PGCGame/PGCGame/CoreTypes/Utilites/PGCParticleGenerator.cs
@@ -16,31 +16,14 @@ namespace PGCGame.CoreTypes.Utilites
         public PGCParticleGenerator(SpriteBatch batch, params Texture2D[] textures) : base(batch, textures)
         {
             _spriteBatch = batch;
+            TTLSettings = TimeToLiveSettings.AlphaLess100;
         }
 
         public override Particle GenerateParticle(Vector2 pos)
         {
-            Particle particle = new Particle(Textures[Random.Next(Textures.Count)], pos, _spriteBatch);
-
-            particle.Speed = new Vector2((float)(Random.NextDouble() * 2 - 1), (float)(Random.NextDouble() * 2 - 1));
-            particle.RotationVelocity = MathHelper.ToDegrees(Convert.ToSingle(Random.NextDouble() * 2 - 1) / 10f);
-            //particle.Color = new Color(_random.Next(255), _random.Next(255), Random.Next(255), _random.Next(255));
-            particle.Scale = new Vector2((float)Random.NextDouble());
-            particle.TimeToLive = TimeSpan.FromTicks(Random.Next((int)MinimumTimeToLive.Ticks, (int)MaximumTimeToLive.Ticks));
-            particle.TimeToLiveSettings = TTLSettings;
-
-            if (MinimumParticleColorChangeRate != 1)
-            {
-                float particleColorDegenerationRate = (float)Random.NextDouble();
-                while (particleColorDegenerationRate < MinimumParticleColorChangeRate)
-                {
-                    particleColorDegenerationRate += Convert.ToSingle(Random.NextDouble() % 0.15);
-                }
-                particle.ColorChange = particleColorDegenerationRate;
-            }
-
-            particle.UseCenterAsOrigin = true;
-
+            Particle particle = base.GenerateParticle(pos);
+            particle.Color = Color.White;
+            particle.ColorChange = 0.985f;
             return particle;
         }
     }
